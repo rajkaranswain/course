bc099280748b168e6cf5ecec72363c2c2a3d701b
rajkaranswain <rajkaranswain16@gmail.com>
2021-01-02 19:33:27 +0530

    1st app on fluttter course

---
+++ firstapp/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+void main() {
+    
+}
+class myApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp();
+  }
+}
