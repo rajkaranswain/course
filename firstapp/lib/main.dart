08cf719a6a053883613d969793914e8741737f43
rajkaranswain <rajkaranswain16@gmail.com>
2021-01-03 01:16:39 +0530

    used row and column for more than one widget

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -11,12 +11,19 @@ class MyApp extends StatelessWidget {
   //MyApp is the core class
   @override
   Widget build(BuildContext context) {
+    var question = ['whats your favraite colour',
+    'what is your fav animal'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('my 1st app'),
         ),
-        body: Text('hello world default text'),
+        body:Column(children:[
+          Text('the question'),
+          RaisedButton(child:Text('answer 1'),onPressed:null),
+          RaisedButton(child:Text('answer 2'),onPressed:null),
+          RaisedButton(child:Text('answer 3'),onPressed:null),
+        ],)
 
       ),
     );
