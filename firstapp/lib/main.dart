7c57d09c1739da381b66a15cf322f0f7e27536aa
rajkaranswain <rajkaranswain16@gmail.com>
2021-01-03 01:06:31 +0530

    added body and app bar

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+
 //includes the needs and dependenceies
 //void main() {
-  //  runApp(MyApp());}
-  void main() => runApp(MyApp());
-    //exposes the material.dart package
-    //tagets the the build method
+//  runApp(MyApp());}
+void main() => runApp(MyApp());
+//exposes the material.dart package
+//tagets the the build method
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   //MyApp is the core class
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      home: Text('hello world !'),
-      
-      );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my 1st app'),
+        ),
+        body: Text('hello world default text'),
+
+      ),
+    );
   }
 }
