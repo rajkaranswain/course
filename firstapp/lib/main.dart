5c1d50c00123f924897acf7f514dc5c34c946408
rajkaranswain <rajkaranswain16@gmail.com>
2021-01-02 20:27:40 +0530

    running the app

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+//includes the needs and dependenceies
 void main() {
-    
+    runApp(MyApp());
+    //exposes the material.dart package
+    //tagets the the build method
 }
-class myApp extends StatelessWidget{
+class MyApp extends StatelessWidget{
+  //MyApp is the core class
   @override
   Widget build(BuildContext context) {
     
-    return MaterialApp(home: Text('hello world'),);
+    return MaterialApp(home: Text('hello world!'),);
   }
 }
